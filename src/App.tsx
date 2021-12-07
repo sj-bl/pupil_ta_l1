19ad4f30c4e0eca1ae4e6762adff1948f13ac5ee
developedBySJ <developedbysj@gmail.com>
2021-12-07 21:00:32 +0530

    Done submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const todos = [
+  { name: "3 Chicken Wings", price: 12.11 },
+  { name: "Chicken Livers and Portuguese Roll", price: 6.59 },
+  { name: "Spicy Mixed Olives (V)", price: 8.99 },
+  { name: "Hummus with PERI-PERI Drizzle (V)", price: 12.49 },
+  { name: "Red Pepper Dip (V)", price: 11 },
+  { name: "Appeteaser Platter", price: 3 },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-2xl mx-auto my-4 font-bold">
+      <h1 className="text-center text-4xl mb-6">Menu</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className="p-4 bg-gray-100 mb-2 rounded-md">
+            <div className="flex justify-between">
+              <p>{t.name}</p>
+              <p>$ {t.price}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
