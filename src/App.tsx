5a2bebfe7420207e7b9e7c434745a4644b9e036b
developedBySJ <developedbysj@gmail.com>
2021-12-08 23:30:35 +0530

    Done submission | level 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,69 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const todos = [
-  { name: "3 Chicken Wings", price: 12.11 },
-  { name: "Chicken Livers and Portuguese Roll", price: 6.59 },
-  { name: "Spicy Mixed Olives (V)", price: 8.99 },
-  { name: "Hummus with PERI-PERI Drizzle (V)", price: 12.49 },
-  { name: "Red Pepper Dip (V)", price: 11 },
-  { name: "Appeteaser Platter", price: 3 },
-];
+interface Todo {
+  name: string;
+  isCompleted: boolean;
+}
 
 function App() {
+  const [todo, setTodo] = useState<Todo[]>([]);
+  const [value, setValue] = useState("");
   return (
     <div className="max-w-2xl mx-auto my-4 font-bold">
-      <h1 className="text-center text-4xl mb-6">Menu</h1>
-      <ul>
-        {todos.map((t) => (
-          <li className="p-4 bg-gray-100 mb-2 rounded-md">
-            <div className="flex justify-between">
-              <p>{t.name}</p>
-              <p>$ {t.price}</p>
-            </div>
-          </li>
-        ))}
+      <h1 className="text-center text-4xl mb-6">Todos</h1>
+      <div>
+        <form
+          className="flex"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (value) {
+              setTodo((prev) => [...prev, { isCompleted: false, name: value }]);
+              setValue("");
+            }
+          }}
+        >
+          <input
+            type="text"
+            value={value}
+            className="p-2 bg-gray-100 rounded-tl rounded-bl w-full focus:border-blue-500 border-transparent border-2 outline-none"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button
+            className="py-2 px-4 bg-blue-500 text-white rounded-tr rounded-br"
+            type="reset"
+          >
+            Add
+          </button>
+        </form>
+      </div>
+      <ul className="mt-4">
+        {todo.map((t, i) => {
+          return (
+            <li
+              className="p-4 bg-gray-100 flex items-center justify-between mb-2 rounded-lg"
+              key={i}
+            >
+              <p className={t.isCompleted ? "line-through" : ""}>{t.name}</p>
+              <button
+                className={
+                  t.isCompleted
+                    ? "py-2 px-4 text-green-500 cursor-default"
+                    : "py-2 px-4 bg-green-500 text-white rounded"
+                }
+                onClick={(_) => {
+                  const newTodo = [...todo];
+                  newTodo[i].isCompleted = true;
+                  newTodo.sort((a, b) => (a.isCompleted ? 1 : -1));
+                  setTodo(newTodo);
+                }}
+                disabled={t.isCompleted}
+              >
+                {t.isCompleted ? "Completed" : "Mark Completed"}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
